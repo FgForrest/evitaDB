34d891b9e73e5319bfc6098f1647ea878e3487ea
Jan Novotný <novotnaci@gmail.com>
2023-03-17 18:03:41 +0100

    docs: schema documentation

--- docs/user/en/use/example/schema-definition.java
+++ docs/user/en/use/example/schema-definition.java
@@ -2,8 +2,12 @@ evita.updateCatalog(
 	"testCatalog",
 	session -> {
 		session.defineEntitySchema("Product")
-			/* all is strictly verified but associated data and references can be added on the fly */
-			.verifySchemaButAllow(EvolutionMode.ADDING_ASSOCIATED_DATA, EvolutionMode.ADDING_REFERENCES)
+			/* all is strictly verified but associated data
+			   and references can be added on the fly */
+			.verifySchemaButAllow(
+				EvolutionMode.ADDING_ASSOCIATED_DATA,
+				EvolutionMode.ADDING_REFERENCES
+			)
 			/* product are not organized in the tree */
 			.withoutHierarchy()
 			/* prices are referencing another entity stored in Evita */
@@ -11,26 +15,61 @@ evita.updateCatalog(
 			/* en + cs localized attributes and associated data are allowed only */
 			.withLocale(Locale.ENGLISH, new Locale("cs", "CZ"))
 			/* here we define list of attributes with indexes for search / sort */
-			.withAttribute("code", String.class, whichIs -> whichIs.unique())
-			.withAttribute("url", String.class, whichIs -> whichIs.unique().localized())
-			.withAttribute("oldEntityUrls", String[].class, whichIs -> whichIs.filterable().localized())
-			.withAttribute("name", String.class, whichIs -> whichIs.filterable().sortable())
-			.withAttribute("ean", String.class, whichIs -> whichIs.filterable())
-			.withAttribute("priority", Long.class, whichIs -> whichIs.sortable())
-			.withAttribute("validity", DateTimeRange.class, whichIs -> whichIs.filterable())
-			.withAttribute("quantity", BigDecimal.class, whichIs -> whichIs.filterable().indexDecimalPlaces(2))
-			.withAttribute("alias", Boolean.class, whichIs -> whichIs.filterable())
-			/* here we define set of associated data, that can be stored along with entity */
+			.withAttribute(
+				"code", String.class,
+				whichIs -> whichIs.unique()
+			)
+			.withAttribute(
+				"url", String.class,
+				whichIs -> whichIs.unique().localized()
+			)
+			.withAttribute(
+				"oldEntityUrls", String[].class,
+				whichIs -> whichIs.filterable().localized()
+			)
+			.withAttribute(
+				"name", String.class,
+				whichIs -> whichIs.filterable().sortable()
+			)
+			.withAttribute(
+				"ean", String.class,
+				whichIs -> whichIs.filterable()
+			)
+			.withAttribute(
+				"priority", Long.class,
+				whichIs -> whichIs.sortable()
+			)
+			.withAttribute(
+				"validity", DateTimeRange.class,
+				whichIs -> whichIs.filterable()
+			)
+			.withAttribute(
+				"quantity", BigDecimal.class,
+				whichIs -> whichIs.filterable().indexDecimalPlaces(2)
+			)
+			.withAttribute(
+				"alias", Boolean.class,
+				whichIs -> whichIs.filterable()
+			)
+			/* here we define set of associated data,
+			   that can be stored along with entity */
 			.withAssociatedData("referencedFiles", ReferencedFileSet.class)
-			.withAssociatedData("labels", Labels.class, whichIs -> whichIs.localized())
-			/* here we define references that relate to another entities stored in Evita */
+			.withAssociatedData(
+				"labels", Labels.class,
+				whichIs -> whichIs.localized()
+			)
+			/* here we define references that relate to
+			   another entities stored in Evita */
 			.withReferenceToEntity(
 				"categories",
 				"Category",
 				Cardinality.ZERO_OR_MORE,
 				whichIs ->
 					/* we can specify special attributes on relation */
-					whichIs.filterable().withAttribute("categoryPriority", Long.class, thatIs -> thatIs.sortable())
+					whichIs.filterable().withAttribute(
+						"categoryPriority", Long.class,
+						thatIs -> thatIs.sortable()
+					)
 			)
 			/* for faceted references we can compute "counts" */
 			.withReferenceToEntity(
@@ -38,7 +77,8 @@ evita.updateCatalog(
 				"Brand",
 				Cardinality.ZERO_OR_ONE,
 				whichIs -> whichIs.faceted())
-			/* references may be also represented be entities unknown to Evita */
+			/* references may be also represented be
+			   entities unknown to Evita */
 			.withReferenceTo(
 				"stock",
 				"stock",
