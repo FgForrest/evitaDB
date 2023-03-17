6e7a452bf4920c201d46dbeaa011a79416684263
Jan Novotný <novotnaci@gmail.com>
2023-03-17 20:57:03 +0100

    docs: schema documentation

---
+++ docs/user/en/use/example/declarative-model-example.java
@@ -0,0 +1,82 @@
+/*
+ *
+ *                         _ _        ____  ____
+ *               _____   _(_) |_ __ _|  _ \| __ )
+ *              / _ \ \ / / | __/ _` | | | |  _ \
+ *             |  __/\ V /| | || (_| | |_| | |_) |
+ *              \___| \_/ |_|\__\__,_|____/|____/
+ *
+ *   Copyright (c) 2023
+ *
+ *   Licensed under the Business Source License, Version 1.1 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   https://github.com/FgForrest/evitaDB/blob/main/LICENSE
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+@Entity(
+	allowedEvolution = {
+		EvolutionMode.ADDING_LOCALES,
+		EvolutionMode.ADDING_CURRENCIES
+	}
+)
+public interface Product {
+
+	@PrimaryKey
+	int getId();
+
+	@Attribute
+	@Nonnull
+	String getCode();
+
+	@Attribute(
+		name = "manufacturedBefore",
+		description = "How many years ago the product was manufactured.",
+		deprecated = "This attribute is obsolete.",
+		filterable = true
+	)
+	@Nonnull
+	default int[] getYears() {
+		// the default implementation defines default value
+		return new int[] {1978,2005,2020};
+	}
+
+	@AssociatedData
+	@Nonnull
+	ReferencedFiles getReferencedFiles();
+
+	@Parent
+	int getParentEntity();
+
+	@SellingPrice
+	PriceContract getSellingPrice();
+
+	@Reference
+	Brand getMarketingBrand();
+
+	@Reference
+	Brand[] getLicensingBrands();
+
+	record ReferencedFiles(@Nonnull int... fileId) {}
+
+	interface Brand extends Serializable {
+
+		@ReferencedEntity
+		int getBrand();
+
+		@ReferencedEntityGroup
+		int getBrandGroup();
+
+		@Attribute
+		String getMarket();
+
+	}
+
+}
