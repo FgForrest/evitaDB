fb5ada5f872cc00ea9d506810e1e73e33d19d6cb
Lukáš Hornych <lukashornych@protonmail.com>
2023-05-25 12:34:00 +0200

    docs: user hierarchy graphql docs (WIP)

--- docs/user/en/query/filtering/examples/hierarchy/hierarchy-within-self-simple.java
+++ docs/user/en/query/filtering/examples/hierarchy/hierarchy-within-self-simple.java
@@ -1,10 +1,10 @@
-// test
 final EvitaResponse<SealedEntity> entities = evita.queryCatalog(
 	"evita",
 	session -> {
 		return session.querySealedEntity(
 			query(
 				collection("Category"),
+				collection(),
 				filterBy(
 					hierarchyWithinSelf(
 						attributeEquals("code", "accessories")
