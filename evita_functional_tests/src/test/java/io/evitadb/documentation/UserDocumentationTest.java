f6240227eaa1e2f382b8e6fff7653a2dbc742aee
Jan Novotný <novotnaci@gmail.com>
2023-05-26 16:26:51 +0200

    docs: entityPrimaryKeyInSet documentation

--- evita_functional_tests/src/test/java/io/evitadb/documentation/UserDocumentationTest.java
+++ evita_functional_tests/src/test/java/io/evitadb/documentation/UserDocumentationTest.java
@@ -319,7 +319,7 @@ public class UserDocumentationTest implements EvitaTestSupport {
 	@Disabled
 	Stream<DynamicTest> testSingleFileDocumentationAndCreateOtherLanguageSnippets() {
 		return this.createTests(
-			getRootDirectory().resolve("docs/user/en/query/ordering/constant.md"),
+			getRootDirectory().resolve("docs/user/en/query/filtering/constant.md"),
 			CreateSnippets.MARKDOWN, CreateSnippets.JAVA
 		).stream();
 	}
