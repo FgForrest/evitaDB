aaf5b503c94206a134e33046c34f8e901aac438e
Lukáš Hornych <lukashornych@proton.me>
2023-08-11 13:39:38 +0200

    fix: replace Path class with custom UriPath because Path uses system-specific delimiters but URLs have always "/"

--- evita_external_api/evita_external_api_rest/src/main/java/io/evitadb/externalApi/rest/RestManager.java
+++ evita_external_api/evita_external_api_rest/src/main/java/io/evitadb/externalApi/rest/RestManager.java
@@ -37,8 +37,9 @@ import io.evitadb.externalApi.rest.api.openApi.OpenApiSystemEndpoint;
 import io.evitadb.externalApi.rest.api.system.SystemRestBuilder;
 import io.evitadb.externalApi.rest.configuration.RestConfig;
 import io.evitadb.externalApi.rest.exception.OpenApiInternalError;
-import io.evitadb.externalApi.rest.io.RestExceptionHandler;
 import io.evitadb.externalApi.rest.io.RestEndpointHandler;
+import io.evitadb.externalApi.rest.io.RestExceptionHandler;
+import io.evitadb.externalApi.utils.UriPath;
 import io.evitadb.utils.Assert;
 import io.evitadb.utils.StringUtils;
 import io.undertow.Handlers;
@@ -52,7 +53,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.nio.file.Path;
 import java.util.Map;
 import java.util.Set;
 
@@ -84,7 +84,7 @@ public class RestManager {
 	 * REST specific endpoint router.
 	 */
 	private final RoutingHandler restRouter = Handlers.routing();
-	@Nonnull private final Map<String, CorsEndpoint> corsEndpoints = createConcurrentHashMap(20);
+	@Nonnull private final Map<UriPath, CorsEndpoint> corsEndpoints = createConcurrentHashMap(20);
 
 	public RestManager(@Nonnull Evita evita, @Nonnull RestConfig restConfig) {
 		this.evita = evita;
@@ -95,7 +95,7 @@ public class RestManager {
 		// register initial endpoints
 		registerSystemApi();
 		this.evita.getCatalogs().forEach(catalog -> registerCatalog(catalog.getName()));
-		corsEndpoints.forEach((path, endpoint) -> restRouter.add("OPTIONS", path, endpoint.toHandler()));
+		corsEndpoints.forEach((path, endpoint) -> restRouter.add("OPTIONS", path.toString(), endpoint.toHandler()));
 
 		log.info("Built REST API in " + StringUtils.formatPreciseNano(System.currentTimeMillis() - buildingStartTime));
 	}
@@ -190,7 +190,7 @@ public class RestManager {
 	private void registerSystemRestEndpoint(@Nonnull Rest.Endpoint endpoint) {
 		registerRestEndpoint(
 			endpoint.method(),
-			Path.of("/" + OpenApiSystemEndpoint.URL_PREFIX).resolve(endpoint.path()),
+			UriPath.of("/", OpenApiSystemEndpoint.URL_PREFIX, endpoint.path()),
 			endpoint.handler()
 		);
 	}
@@ -198,8 +198,8 @@ public class RestManager {
 	/**
 	 * Registers endpoints into router. Also CORS endpoint is created automatically for this endpoint.
 	 */
-	private void registerRestEndpoint(@Nonnull HttpString method, @Nonnull Path path, @Nonnull RestEndpointHandler<?, ?> handler) {
-		final CorsEndpoint corsEndpoint = corsEndpoints.computeIfAbsent(path.toString(), p -> new CorsEndpoint(restConfig));
+	private void registerRestEndpoint(@Nonnull HttpString method, @Nonnull UriPath path, @Nonnull RestEndpointHandler<?, ?> handler) {
+		final CorsEndpoint corsEndpoint = corsEndpoints.computeIfAbsent(path, p -> new CorsEndpoint(restConfig));
 		corsEndpoint.addMetadataFromHandler(handler);
 
 		restRouter.add(
@@ -218,8 +218,8 @@ public class RestManager {
 	}
 
 	@Nonnull
-	private Path constructCatalogPath(@Nonnull CatalogContract catalog, @Nonnull Path endpointPath) {
-		return Path.of("/" + catalog.getSchema().getNameVariant(URL_NAME_NAMING_CONVENTION)).resolve(endpointPath);
+	private UriPath constructCatalogPath(@Nonnull CatalogContract catalog, @Nonnull UriPath endpointPath) {
+		return UriPath.of(catalog.getSchema().getNameVariant(URL_NAME_NAMING_CONVENTION), endpointPath);
 	}
 
 	private static class CorsEndpoint {
