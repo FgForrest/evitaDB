58fc068a842eefe5cdb4e889c5156225c7a4641d
Jan Novotný <novotnaci@gmail.com>
2023-08-24 09:32:54 +0200

    doc: price content documentation

---
+++ documentation/user/en/query/requirements/examples/fetching/dataInLocalesAll.graphql.json.md
@@ -0,0 +1,18 @@
+```json
+{
+  "data" : [
+    {
+      "primaryKey" : 64703,
+      "attributesGlobal" : {
+        "code" : "samsung"
+      },
+      "attributesDe" : {
+        "name" : "Samsung"
+      },
+      "attributesCs" : {
+        "name" : "Samsung"
+      }
+    }
+  ]
+}
+```
---
+++ documentation/user/en/query/requirements/examples/fetching/priceContent.java
@@ -0,0 +1,21 @@
+final EvitaResponse<SealedEntity> entities = evita.queryCatalog(
+	"evita",
+	session -> {
+		return session.querySealedEntity(
+			query(
+				collection("Product"),
+				filterBy(
+					entityPrimaryKeyInSet(103885),
+					priceInCurrency(Currency.getInstance("EUR")),
+					priceInPriceLists("employee-basic-price", "basic")
+				),
+				require(
+					entityFetch(
+						attributeContent("code", "name"),
+						priceContentRespectingFilter("reference")
+					)
+				)
+			)
+		);
+	}
+);
