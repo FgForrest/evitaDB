780a2bcb2db0fb08579a926690925662aabef031
Lukáš Hornych <lukashornych@proton.me>
2023-09-15 09:56:47 +0200

    docs: add exception for `local` prop into UserDocumentationTest

--- evita_functional_tests/src/test/java/io/evitadb/documentation/UserDocumentationTest.java
+++ evita_functional_tests/src/test/java/io/evitadb/documentation/UserDocumentationTest.java
@@ -343,7 +343,7 @@ public class UserDocumentationTest implements EvitaTestSupport {
 	@Disabled
 	Stream<DynamicTest> testSingleFileDocumentation() {
 		return this.createTests(
-			getRootDirectory().resolve("documentation/user/en/query/requirements/fetching.md")
+			getRootDirectory().resolve("documentation/user/en/use/api/query-data.md")
 		).stream();
 	}
 
@@ -425,7 +425,7 @@ public class UserDocumentationTest implements EvitaTestSupport {
 
 		final Matcher sourceCodeTabsMatcher = SOURCE_CODE_TABS_PATTERN.matcher(fileContent);
 		while (sourceCodeTabsMatcher.find()) {
-			final Path referencedFile = createPathRelativeToRootDirectory(rootDirectory, sourceCodeTabsMatcher.group(4));
+			final Path referencedFile = createPathRelativeToRootDirectory(rootDirectory, sourceCodeTabsMatcher.group(5));
 			final String referencedFileExtension = getFileNameExtension(referencedFile);
 			// todo lho: temporary skip testing of source code tab if we dont support its current execution yet
 			if (ofNullable(sourceCodeTabsMatcher.group(2)).map(it -> it.contains("ignoreTest")).orElse(false)) {
