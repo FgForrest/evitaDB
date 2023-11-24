19ae1e746bcd74499e09c65db37591afc64b877b
Lukáš Hornych <lukashornych@proton.me>
2023-11-24 15:36:50 +0100

    perf(#340): optimize validating classifiers against keywords

--- evita_common/src/main/java/io/evitadb/utils/ClassifierUtils.java
+++ evita_common/src/main/java/io/evitadb/utils/ClassifierUtils.java
@@ -29,11 +29,11 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import javax.annotation.Nonnull;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Helper methods for manipulating classifiers (entity types, attribute names, reference names, ...).
@@ -95,6 +95,23 @@ public class ClassifierUtils {
 			"self" // field in hierarchy parents and statistics extra result in GraphQL API, would collide with hierarchy references
 		)
 	);
+	/**
+	 * Normalized version of {@link #RESERVED_KEYWORDS} that is case and separator insensitive. This is so that we
+	 * don't have to compute it for every case for every checked classifier.
+	 */
+	private static final Map<ClassifierType, Set<String>> NORMALIZED_RESERVED_KEYWORDS;
+	static {
+		NORMALIZED_RESERVED_KEYWORDS = RESERVED_KEYWORDS.entrySet()
+			.stream()
+			.map(entry -> Map.entry(
+				entry.getKey(),
+				entry.getValue()
+					.stream()
+					.map(ClassifierUtils::normalizeClassifier)
+					.collect(Collectors.toUnmodifiableSet())
+			))
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	}
 
 	private static final Pattern SUPPORTED_FORMAT_PATTERN = Pattern.compile("(^[\\p{Alpha}][\\p{Alnum}_.\\-~]{0,254}$)");
 
@@ -137,18 +154,20 @@ public class ClassifierUtils {
 		if (classifier.isBlank()) {
 			return false;
 		}
-		return Optional.ofNullable(RESERVED_KEYWORDS.get(classifierType))
-			.map(keywords -> keywords.stream()
-				.flatMap(keyword -> Arrays.stream(NamingConvention.values())
-					.map(namingConvention -> StringUtils.toSpecificCase(keyword, namingConvention)))
-				.anyMatch(keyword -> keyword.equals(classifier) ||
-					keyword.equals(StringUtils.toCamelCase(classifier)) ||
-					keyword.equals(StringUtils.toPascalCase(classifier)) ||
-					keyword.equals(StringUtils.toSnakeCase(classifier)) ||
-					keyword.equals(StringUtils.toUpperSnakeCase(classifier)) ||
-					keyword.equals(StringUtils.toKebabCase(classifier))
-				))
+		final String normalizedClassifier = normalizeClassifier(classifier);
+		return Optional.ofNullable(NORMALIZED_RESERVED_KEYWORDS.get(classifierType))
+			.map(keywords -> keywords.contains(normalizedClassifier))
 			.orElse(false);
 	}
 
+	/**
+	 * Converts classifier to comparable format (lower case, no separators).
+	 */
+	@Nonnull
+	private static String normalizeClassifier(@Nonnull String classifier) {
+		return StringUtils.splitStringWithCaseIntoWords(classifier)
+			.stream()
+			.map(String::toLowerCase)
+			.collect(Collectors.joining());
+	}
 }
