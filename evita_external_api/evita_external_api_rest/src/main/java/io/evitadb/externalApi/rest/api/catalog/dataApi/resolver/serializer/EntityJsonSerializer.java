b2c55d0ca32fb948e29d6fbbb90d7debc8ea226b
Jan Novotný <novotnaci@gmail.com>
2024-04-10 16:29:07 +0200

    feat(#16): updated configuration options documentation

--- evita_external_api/evita_external_api_rest/src/main/java/io/evitadb/externalApi/rest/api/catalog/dataApi/resolver/serializer/EntityJsonSerializer.java
+++ evita_external_api/evita_external_api_rest/src/main/java/io/evitadb/externalApi/rest/api/catalog/dataApi/resolver/serializer/EntityJsonSerializer.java
@@ -356,7 +356,7 @@ public class EntityJsonSerializer {
 
 		final boolean hasMultiplePricesForSale;
 		final PriceInnerRecordHandling priceInnerRecordHandling = entity.getPriceInnerRecordHandling();
-		if (priceInnerRecordHandling.equals(PriceInnerRecordHandling.FIRST_OCCURRENCE)) {
+		if (priceInnerRecordHandling.equals(PriceInnerRecordHandling.LOWEST_PRICE)) {
 			if (allPricesForSale.size() <= 1) {
 				return false;
 			}
