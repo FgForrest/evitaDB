da923085c2c7b4008cdadd59891c8f5e488a8acf
Jan Novotný <novotnaci@gmail.com>
2025-02-04 14:19:14 +0100

    feat: Facet summary integration with hierarchical entities
    When selecting the category XY, the filter will also select all of its child categories when the includingChildren(true) sub-constraint is used.
    
    Refs: #352

---
+++ evita_query/src/main/java/io/evitadb/api/query/filter/FacetIncludingChildren.java
@@ -0,0 +1,44 @@
+/*
+ *
+ *                         _ _        ____  ____
+ *               _____   _(_) |_ __ _|  _ \| __ )
+ *              / _ \ \ / / | __/ _` | | | |  _ \
+ *             |  __/\ V /| | || (_| | |_| | |_) |
+ *              \___| \_/ |_|\__\__,_|____/|____/
+ *
+ *   Copyright (c) 2025
+ *
+ *   Licensed under the Business Source License, Version 1.1 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   https://github.com/FgForrest/evitaDB/blob/master/LICENSE
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package io.evitadb.api.query.filter;
+
+
+import io.evitadb.api.query.descriptor.ConstraintDomain;
+import io.evitadb.api.query.descriptor.annotation.ConstraintDefinition;
+
+/**
+ * TODO JNO
+ *
+ * <p><a href="https://evitadb.io/documentation/query/filtering/references#including-children">Visit detailed user documentation</a></p>
+ *
+ * @author Jan Novotný (novotny@fg.cz), FG Forrest a.s. (c) 2025
+ */
+@ConstraintDefinition(
+	name = "includingChildren",
+	shortDescription = "TODO JNO ",
+	userDocsLink = "/documentation/query/filtering/references#including-children",
+	supportedIn = ConstraintDomain.FACET
+)
+public class FacetIncludingChildren  {
+}
