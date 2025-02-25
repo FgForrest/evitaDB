a8ecf9a9be4ea445d3ca0018e3b3d03d852c1096
Jan Novotný <novotnaci@gmail.com>
2025-02-25 14:41:23 +0100

    fix: Reference with name X in entity Y is not indexed in scope LIVE and cannot be filtered by. Filtering by without index would be slow.
    Error was incorrectly issued when engine fails to find non existing index in correctly indexed scope.
    
    Ref: #815

--- evita_engine/src/main/java/io/evitadb/core/query/ReferencedEntityFetcher.java
+++ evita_engine/src/main/java/io/evitadb/core/query/ReferencedEntityFetcher.java
@@ -6,7 +6,7 @@
  *             |  __/\ V /| | || (_| | |_| | |_) |
  *              \___| \_/ |_|\__\__,_|____/|____/
  *
- *   Copyright (c) 2023-2024
+ *   Copyright (c) 2023-2025
  *
  *   Licensed under the Business Source License, Version 1.1 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -520,12 +520,22 @@ public class ReferencedEntityFetcher implements ReferenceFetcher {
 						)
 					),
 					executionContext.getQueryContext().getScopes(),
-					(es, eik) -> ReferencedTypeEntityIndex.createThrowingStub(es, eik, allReferencedEntityIds),
+					(es, eik) -> {
+						if (referenceSchema.isIndexedInScope(eik.scope())) {
+							return null;
+						} else {
+							return ReferencedTypeEntityIndex.createThrowingStub(es, eik, allReferencedEntityIds);
+						}
+					},
 					(es, eik) -> {
 						final int[] epks = entityPrimaryKeys.get(eik.scope());
-						return ReducedEntityIndex.createThrowingStub(
-							es, eik, epks == null ? ArrayUtils.EMPTY_INT_ARRAY : epks
-						);
+						if (referenceSchema.isIndexedInScope(eik.scope())) {
+							return null;
+						} else {
+							return ReducedEntityIndex.createThrowingStub(
+								es, eik, epks == null ? ArrayUtils.EMPTY_INT_ARRAY : epks
+							);
+						}
 					}
 				);
 
