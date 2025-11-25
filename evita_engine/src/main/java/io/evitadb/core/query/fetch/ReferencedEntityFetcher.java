9c45b69414a9490589bc8d2e0c246a383f322733
Jan Novotný <novotnaci@gmail.com>
2025-11-25 11:24:54 +0100

    fix: fixed named chunk accessor handling
    Correct chunk transformer was not used in named reference set logic.
    
    Refs: #902

--- evita_engine/src/main/java/io/evitadb/core/query/fetch/ReferencedEntityFetcher.java
+++ evita_engine/src/main/java/io/evitadb/core/query/fetch/ReferencedEntityFetcher.java
@@ -1467,6 +1467,8 @@ public class ReferencedEntityFetcher implements ReferenceFetcher {
 				final ReferenceContentKey rck = namedEntry.getKey();
 				final RequirementContext namedRequirementContext = namedEntry.getValue();
 				if (namedRequirementContext.requiresInit()) {
+					final ChunkTransformer namedChunkTransformer = namedEntry.getValue().referenceChunkTransformer();
+					final ChunkTransformerAccessor namedChunkTransformerAccessor =referenceName -> namedChunkTransformer;
 					this.namedFetchedEntities.put(
 						rck.instanceName(),
 						new ReferencedSetEntityFetcher(
@@ -1486,7 +1488,7 @@ public class ReferencedEntityFetcher implements ReferenceFetcher {
 									filterByVisitor
 								)
 							),
-							this.chunkTransformerAccessor
+							namedChunkTransformerAccessor
 						)
 					);
 				}
