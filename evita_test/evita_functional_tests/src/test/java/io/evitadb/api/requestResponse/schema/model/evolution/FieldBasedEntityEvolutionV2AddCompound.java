d0226c3a0261317fbe1ba48c9dad0c5af7343761
Jan Novotný <novotnaci@gmail.com>
2026-01-12 19:05:48 +0100

    feat: ClassSchemaAnalyzer generates superfluous schema mutations
    - Refactored schema mutation methods to apply updates conditionally based on comparisons.
    - Implemented new utility functions for deep comparison of arrays and strings.
    - Enhanced handling of attribute properties, including uniqueness, localization, and default values.
    - Streamlined cardinality determination and reference configuration logic.
    
    Refs: #1059
    Signed-off-by: Jan Novotný <novotnaci@gmail.com>

---
+++ evita_test/evita_functional_tests/src/test/java/io/evitadb/api/requestResponse/schema/model/evolution/FieldBasedEntityEvolutionV2AddCompound.java
@@ -0,0 +1,57 @@
+/*
+ *
+ *                         _ _        ____  ____
+ *               _____   _(_) |_ __ _|  _ \| __ )
+ *              / _ \ \ / / | __/ _` | | | |  _ \
+ *             |  __/\ V /| | || (_| | |_| | |_) |
+ *              \___| \_/ |_|\__\__,_|____/|____/
+ *
+ *   Copyright (c) 2023-2026
+ *
+ *   Licensed under the Business Source License, Version 1.1 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   https://github.com/FgForrest/evitaDB/blob/master/LICENSE
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package io.evitadb.api.requestResponse.schema.model.evolution;
+
+import io.evitadb.api.requestResponse.data.annotation.Attribute;
+import io.evitadb.api.requestResponse.data.annotation.AttributeSource;
+import io.evitadb.api.requestResponse.data.annotation.SortableAttributeCompound;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.annotation.Nonnull;
+
+/**
+ * V2 evolution: adds a sortable attribute compound 'codeNameCompound'.
+ * Also adds 'name' attribute required for the compound.
+ *
+ * @author Jan Novotný (novotny@fg.cz), FG Forrest a.s. (c) 2026
+ */
+@SortableAttributeCompound(
+	name = "codeNameCompound",
+	attributeElements = {
+		@AttributeSource(attributeName = "code"),
+		@AttributeSource(attributeName = "name")
+	}
+)
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class FieldBasedEntityEvolutionV2AddCompound extends FieldBasedEntityEvolutionV1 {
+
+	@Attribute
+	@Nonnull
+	private String name;
+
+}
